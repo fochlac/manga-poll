bbb3a14c12a1d3344b23182df6fd6009f998ab5b
Florian Riedel <florian.riedel@webfleet.com>
2021-08-31 16:46:57 +0200

    add query for impressum

--- src/webapp/impressum.js
+++ src/webapp/impressum.js
@@ -3,6 +3,11 @@ export function addImpressumListeners () {
     const bar = document.getElementById('impressum-bar')
     const impressum = document.getElementById('impressum-wrap')
 
+    const urlParams = new URLSearchParams(window.location.search)
+    if (urlParams.has('impressum')) {
+        impressum.style.display = 'block'
+    }
+
     bar.addEventListener('click', () => {
         impressum.style.display = 'block'
     })
