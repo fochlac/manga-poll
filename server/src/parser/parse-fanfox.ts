8679e81e13e8f63ede4373040cdb672643cbb4d7
Florian Riedel <florian.riedel@webfleet.com>
2021-09-15 22:43:00 +0200

    add browser-headers

--- server/src/parser/parse-fanfox.ts
+++ server/src/parser/parse-fanfox.ts
@@ -5,6 +5,20 @@ import { logWarning } from '../stats'
 import { getUrlKey, getUrls, updateUrl } from '../url-storage'
 
 const TYPE = 'fanfox'
+const headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9",
+    "dnt": "1",
+    "sec-ch-ua": '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+}
 
 function parseFanfox(source: Source, body) {
     const $ = cheerio.load(body)
@@ -46,7 +60,7 @@ function parseFanfox(source: Source, body) {
 
 async function fetchFanFox(source: Source) {
     try {
-        const body = await fetch(source.url, { method: 'get', headers: { cookie: 'isAdult=1;' } }).then((res) => res.text())
+        const body = await fetch(source.url, { method: 'get', headers: { ...headers, cookie: 'isAdult=1;' } }).then((res) => res.text())
 
         return parseFanfox(source, body)
     }
@@ -59,7 +73,7 @@ async function fetchFanFox(source: Source) {
 
 
 async function parseFanfoxPage(rawUrl: string) {
-    const sourcehtml: string = await fetch(rawUrl).then(res => res.text())
+    const sourcehtml: string = await fetch(rawUrl, { headers: { ...headers, cookie: 'isAdult=1;' }}).then(res => res.text())
 
     const $ = cheerio.load(sourcehtml)
 
--- server/src/parser/parse-madaro.ts
+++ server/src/parser/parse-madaro.ts
@@ -7,7 +7,20 @@ import { getUrlKey, getUrls, updateUrl } from '../url-storage'
 
 const TYPE = 'madara'
 
-const warned: Record<string, number> = {}
+const headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9",
+    "dnt": "1",
+    "sec-ch-ua": '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+}
 
 const dateMonthFirst = /^[^\d]*(1[012]|0\d|\d)[^\d](3[0,1]|[012]\d|\d)[^\d](\d{2}|\d{4})[^\d]*$/
 const dateDayFirst = /^[^\d]*(3[0,1]|[012]\d|\d)[^\d](1[012]|0\d|\d)[^\d](\d{2}|\d{4})[^\d]*$/
@@ -132,7 +145,7 @@ function decodeHTMLEntities(str) {
 }
 
 async function parseMadaroPage(rawUrl: string) {
-    const sourcehtml: string = await fetch(rawUrl).then(res => res.text())
+    const sourcehtml: string = await fetch(rawUrl, { headers }).then(res => res.text())
     const $ = cheerio.load(sourcehtml)
 
     const ids = [
@@ -190,14 +203,14 @@ async function fetchMadaro(source: Source) {
         formData.append('manga', source.mangaId)
         const baseurl = source.url.match(/https?:\/\/[^/]*\//)?.[0]
 
-        let body = await fetch(`${baseurl}wp-admin/admin-ajax.php`, { method: 'post', body: formData }).then((res) => res.text())
+        let body = await fetch(`${baseurl}wp-admin/admin-ajax.php`, { method: 'post', body: formData, headers }).then((res) => res.text())
 
         if (source.id === 'Mc5-xsEjqz') {
             console.log(body.length, '\n', body)
         }
 
         if (body.length < 1000) {
-            body = await fetch(source.url).then((res) => res.text())
+            body = await fetch(source.url, { headers }).then((res) => res.text())
         }
 
         return parseMadaro(source, body)
