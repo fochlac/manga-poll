2b9b929ac09c0bd379cb817f02789b8d91462456
Florian Riedel <florian.riedel@webfleet.com>
2021-09-15 23:27:22 +0200

    fix levi

--- server/src/parser/parse-madara.ts
+++ server/src/parser/parse-madara.ts
@@ -201,7 +201,7 @@ async function fetchMadara(source: Source) {
     try {
         if (source.url.includes('leviatanscans.com')) {
             body = await fetch(source.url, { method: 'post', headers }).then((res) => res.text())
-            console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n',source.url, '>>>>>>>>>>>>>>>>>>>>>>>>>>> \n', body.replace('\n', ' '))
+            console.log('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n',source.url, '\n>>>>>>>>>>>>>>>>>>>>>>>>>>> \n\n\n', body.replace('\n', ' '))
             return parseMadara(source, body)
         }
         const formData = new FormData()
