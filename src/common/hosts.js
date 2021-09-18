c38e94aad0911e47d8a9434278681eaa6eb5e48b
Florian Riedel <florian.riedel@webfleet.com>
2021-09-18 12:23:26 +0200

    add note on madara theme

--- src/common/hosts.js
+++ src/common/hosts.js
@@ -7,7 +7,7 @@ export async function renderHostList (_db, api) {
 
         const hostList = hosts.stable
             .sort((a, b) => String(a?.name).localeCompare(b?.name))
-            .map((host) => `<a href="${host.url}">${host.name}</a>`).join('<span>, </span>')
+            .map((host) => `<a href="${host.url}" target="_blank" rel="noopener">${host.name}</a>`).join('<span>, </span>')
         hostContainer.innerHTML = `
             <h6>Supported Pages</h6>
             <div class="link-list">${hostList}</div>
@@ -16,11 +16,14 @@ export async function renderHostList (_db, api) {
         if (hosts.unstable.length) {
             const hostList = hosts.unstable
                 .sort((a, b) => String(a?.name).localeCompare(b?.name))
-                .map((host) => `<a href="${host.url}">${host.name}</a>`).join('<span>, </span>')
+                .map((host) => `<a href="${host.url}" target="_blank" rel="noopener">${host.name}</a>`).join('<span>, </span>')
             hostContainer.innerHTML += `
                 <p>These Pages had some problems recently &ndash; they might or might not work:</p>
                 <div class="link-list">${hostList}</div>
             `
         }
+        hostContainer.innerHTML += `
+            <p>Many other pages can work as well, if they use the <a href="https://themeforest.net/item/madara-wordpress-theme-for-manga/20849828" target="_blank" rel="noopener">Madara-Theme</a>.</p>
+        `
     }
 }
