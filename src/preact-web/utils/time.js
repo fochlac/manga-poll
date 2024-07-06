b37c6a99414ecc22cf79bc1bf3ce2af2b3e59bfe
Florian Riedel <low@fochlac.com>
2024-07-07 00:06:26 +0200

    fix time for current date

--- src/preact-web/utils/time.js
+++ src/preact-web/utils/time.js
@@ -5,13 +5,13 @@ const hourInMs = 60 * minInMs
 const dayInMs = 24 * hourInMs
 
 export function formatTime (timestamp) {
-    const diff = new Date().setHours(0, 0, 0, 0) - timestamp
+    const date = new Date(Number(timestamp))
+    const diff = new Date().setHours(24, 0, 0, 0) - timestamp
 
     if (diff < dayInMs) {
         return `${date.getHours()}:${pad(date.getMinutes())}`
     }
 
-    const date = new Date(Number(timestamp))
     return `${pad(date.getDate())}.${pad(date.getMonth() + 1)}`
 }
 
