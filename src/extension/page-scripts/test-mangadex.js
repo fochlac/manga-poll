7a0c34da96c88a42c11a9e26225f0d0a67e6dd26
Florian Riedel <florian.riedel@webfleet.com>
2024-09-13 10:19:17 +0200

    fix extension for mangadex

--- src/extension/page-scripts/test-mangadex.js
+++ src/extension/page-scripts/test-mangadex.js
@@ -1,7 +1,7 @@
 export function testMangadex () {
     if (/title\/[\d-\w]*(\/[\d-\w]*|)$/.test(window.location.pathname)) {
         const id = window.location.pathname.split('/')?.[2]
-        const name = document.querySelector('.manga-container .title p')?.innerText?.trim()
+        const name = document.querySelector('.manga .title p')?.innerText?.trim()
 
         return {
             type: 'mangadex',
@@ -10,11 +10,11 @@ export function testMangadex () {
             url: id ? `https://api.mangadex.org/manga/${id}` : null
         }
     }
-    else if (/chapter\/[\d-\w]*\/\d*/.test(window.location.pathname)) {
+    else if (/chapter\/[\d-\w]*(\/\d*|)/.test(window.location.pathname)) {
         const link = document.querySelector('a.text-primary[href*="/title/"]')
         const name = link?.innerText?.trim()
         const id = link?.href.split('/')?.[4]
-        const chapter = document.querySelector('.menu').innerText.match(/Ch. (\d+)/)?.[1]
+        const chapter = document.querySelector('.chapter').innerText.match(/Ch. (\d+)/)?.[1]
 
         return {
             type: 'mangadex',
